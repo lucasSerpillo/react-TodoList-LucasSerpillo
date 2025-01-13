e8a3370c10cd4c9f36629eddc62a278e50d25e22
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2025-01-13 10:20:48 +0000

    Todo tareas basicas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+
+	const [todo, setTodo] = useState([])
+	const [Task, setTask] = useState("")
+
+
+	const handleAddList = () => {
+		const newList = [...todo, Task]
+		setTodo(newList)
+		setTask("")
+	}
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			handleAddList();
+		}
+	};
+
+	const handleDelete = (taskToDelete) => {
+		const newList = todo.filter((task) => task !== taskToDelete);
+		setTodo(newList);
+	};
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="text-center"><h1 className="text-center mt-5 display-1">TODOS</h1></div>
+
+			<div className="container w-50">
+				<ul className="list-group rounded-0 shadow">
+					<li className="list-group-item">
+						<input
+							onKeyDown={handleKeyDown}
+							value={Task}
+							onChange={(e) => setTask(e.target.value)}
+							className="form-control"
+							placeholder="Agregar Tarea"
+						/>
+					</li>
+					
+					{todo.length === 0 ? (
+						<li className="list-group-item text-center">
+							<p>No tienes tareas pendientes</p>
+						</li>
+					) : (
+						todo.map((task, index) => (
+							<li key={index} className="list-group-item d-flex justify-content-between task">
+								{task}
+								<button onClick={() => handleDelete(task)} className="btn btn-danger btn-sm deleteButton"> X </button>
+							</li>
+						))
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 };
+
+
 
 export default Home;
